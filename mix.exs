cf1349b6bddb316b9f62213855b292a8016b6608
Marcos Javier Magni Mattoni <javier.magni@gmail.com>
2025-03-09 10:52:32 +0100

    Add cowboy package

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Hello.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:plug_cowboy, "2.7.3"},
     ]
   end
 
